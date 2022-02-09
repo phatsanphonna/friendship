28b61eb9d1dcf912b7001eda1f61b4a5987a545f
ssuniie <phatsanphon2004@outlook.com>
2022-02-10 00:14:56 +0700

    fix postContent return status code 502

--- utils/postContent.ts
+++ utils/postContent.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { pushWebhook } from './webhook';
 
 type Post = {
     name: string,
@@ -7,8 +8,9 @@ type Post = {
 
 export const postContent = async (post: Post) => {
     try {
+        alert('ส่งเฟรนด์ชิปแล้วเรียบร้อย!')
         const { data, status } = await axios.post('/api/content', post, { headers: { 'Content-Type': 'Application/JSON' } })
-        alert(data.message)
+        await pushWebhook(post)
         return { data, status }
     } catch (err) {
         console.log(err)
---
+++ utils/webhook.ts
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+type Post = {
+    name: string,
+    message: string,
+}
+
+export const pushWebhook = async (data: Post) => {
+    await axios.post('https://discord.com/api/webhooks/941018696846037022/2jBuU6Ry8PoEdRIkVHjxrS7K1ur2z5DlH_k5SnNwjy-PtHFD9zaDZgtf4WBj460c7viT', {
+        content: `name: ${data.name}
+        message: ${data.message}
+        `
+    })
+}
